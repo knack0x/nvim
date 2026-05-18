352e584460a91713c378d10b67554d9818c1a538
Udo Bauer <knackwurstking.tux@gmail.com>
2026-05-18 14:13:48 +0200

    Add af keymap, binding for the shift+tab keymap in agentic plugin

--- lua/keymaps/agentic.lua
+++ lua/keymaps/agentic.lua
@@ -1,34 +1,39 @@
 -- Toggle Agentic Chat
 vim.keymap.set({ "n", "v", "i" }, "<C-\\>", function()
-  require("agentic").toggle()
+	require("agentic").toggle()
 end, { desc = "Toggle Agentic Chat" })
 
 -- Add file or selection to Context
 vim.keymap.set({ "n", "v" }, "<C-'>", function()
-  require("agentic").add_selection_or_file_to_context()
+	require("agentic").add_selection_or_file_to_context()
 end, { desc = "Add to Agentic Context" })
 
 -- New Session
 vim.keymap.set({ "n" }, "<space>an", function()
-  require("agentic").new_session()
+	require("agentic").new_session()
 end, { desc = "New Agentic Session" })
 
 -- Restore Session
 vim.keymap.set({ "n" }, "<space>ar", function()
-  require("agentic").restore_session()
+	require("agentic").restore_session()
 end, { desc = "Agentic Restore session", silent = true })
 
 -- Line Diagnostics
 vim.keymap.set("n", "<leader>ad", function()
-  require("agentic").add_current_line_diagnostics()
+	require("agentic").add_current_line_diagnostics()
 end, { desc = "Add line diagnostic to Agentic" })
 
 -- Buffer Diagnostics
 vim.keymap.set("n", "<leader>aD", function()
-  require("agentic").add_buffer_diagnostics()
+	require("agentic").add_buffer_diagnostics()
 end, { desc = "Add all buffer diagnostics to Agentic" })
 
 -- Stop Generation
 vim.keymap.set("n", "<leader>as", function()
-  require("agentic").stop_generation()
+	require("agentic").stop_generation()
 end, { desc = "Stop current Agentic generation" })
+
+
+-- Shift-Tab auf <leader>af binden
+vim.keymap.set({ 'n', 'v' }, '<leader>af', '<S-Tab>',
+	{ desc = 'Agentic shift+tab zu <leader>af wechseln', remap = true })
