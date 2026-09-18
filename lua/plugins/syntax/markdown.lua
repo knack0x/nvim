fad05c1fd8d882587db6a82abfc5cc2834d73cdb
Udo Bauer <knackwurstking.tux@gmail.com>
2026-09-18 22:49:22 +0200

    feat(markdown): re-enable render-markdown and use gruvbox preview theme

--- lua/plugins/syntax/markdown.lua
+++ lua/plugins/syntax/markdown.lua
@@ -7,16 +7,16 @@ vim.g.mkdp_auto_close = 1
 vim.g.mkdp_refresh_slow = 0
 vim.g.mkdp_command_for_global = 0
 --vim.g.mkdp_browser = "firefox"
---vim.g.mkdp_theme = "gruvbox"
+vim.g.mkdp_theme = "gruvbox"
 
 -- NOTE: Disabled for now
 --
---vim.pack.add({
---    'https://github.com/nvim-treesitter/nvim-treesitter',
---    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
---    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
---    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
---    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
---})
---
---require('render-markdown').setup({}) -- only mandatory if you want to set custom options
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+    'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+    'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+
+require('render-markdown').setup({}) -- only mandatory if you want to set custom options
